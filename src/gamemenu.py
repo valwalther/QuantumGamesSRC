36a376f7426ccb2231f96413f33ca04f4b196e39
Valentin Walther <info123@gmx-topmail.de>
2023-09-14 11:20:11 -0400

    changed gamemenu

--- src/gamemenu.py
+++ src/gamemenu.py
@@ -4,6 +4,6 @@ print("TEST!")
 
 def get_menu():
     
-    mdtext = '# Here are our Quantum Games: <p style="color:blue;font-size:18px;"><large><a href=./games/stiritup.ipynb> Stir it up </a></large></p> </br> <p style="color:blue;font-size:18px;">Atomic shuttle</large></p> </br> <p style="color:blue;font-size:18px;">Algorithm puzzle</large></p>"'
+    mdtext = '# Here are our Quantum Games: <p style="color:blue;font-size:18px;"><large><a href=./games/stiritup.ipynb> Stir it up </a></large></p> </br> <p style="color:blue;font-size:18px;">Atomic shuttle</large></p> </br> <p style="color:blue;font-size:18px;">Algorithm puzzle</large></p>'
     
     return mdtext

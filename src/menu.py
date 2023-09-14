b794f1b1c3366fb48b5c95502fa258f032053fb6
Valentin Walther <info123@gmx-topmail.de>
2023-09-14 10:44:42 -0400

    menu changes

--- src/menu.py
+++ src/menu.py
@@ -1,7 +1,7 @@
 from IPython.display import display_markdown
 
-def print_menu():
+def get_menu():
     
     mdtext = '# Here are our Quantum Games: <p style="color:blue;font-size:18px;"><large><a href=./games/stiritup.ipynb> Stir it up </a></large></p> </br> <p style="color:blue;font-size:18px;">Atomic shuttle</large></p> </br> <p style="color:blue;font-size:18px;">Algorithm puzzle</large></p>"'
     
-    display_markdown(mdtext)
+    return mdtext
